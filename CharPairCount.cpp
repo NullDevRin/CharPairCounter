485324261d69fa28d8194780762874f65311b031
unknown <NullDevRin@users.noreply.github.com>
2023-08-06 19:37:39 -0700

    sort all pair counts and keep track of indexes

--- CharPairCount.cpp
+++ CharPairCount.cpp
@@ -2,12 +2,23 @@
 #include <fstream>
 #include <string>
 #include <array>
+#include <algorithm> 
 
 #define PAIR_SIZE 2 
 
+struct indexValuePair{
+    unsigned short index = 0;
+    unsigned int value = 0;
+};
+
+bool indexValuePairCompare (indexValuePair &i,indexValuePair &j) { return (i.value > j.value); }
+
 int main(){
 
-    std::array<unsigned int, 26*26> charPairCounts{0};
+    std::array<indexValuePair, 26*26> charPairCounts{};
+    for(int i = 1; i < charPairCounts.size(); i++){
+        charPairCounts[i].index += i;
+    }
 
     std::ifstream file("wordlistNoSpecials.txt");
 
@@ -28,7 +39,7 @@ int main(){
             if(pairIndex > 26*26){
                 printf("%s%s", word[i-1], word[i]);
             }
-            charPairCounts[pairIndex]++;
+            charPairCounts[pairIndex].value++;
         }
     }
     file.close();
@@ -36,7 +47,14 @@ int main(){
     for(int i=0; i < charPairCounts.size(); i++){
         unsigned char charValue1 = i/26;
         unsigned char charValue2 = i - charValue1*26;
-        printf("%d:%c%c: %d\n", i+1, charValue1+'a', charValue2+'a', charPairCounts[i]);
+        printf("%d:%c%c: %d\n", i+1, charValue1+'a', charValue2+'a', charPairCounts[i].value);
+    }
+
+    std::sort(charPairCounts.begin(), charPairCounts.end(), indexValuePairCompare);
+    for(int i=0; i < charPairCounts.size(); i++){
+        unsigned char charValue1 = charPairCounts[i].index/26;
+        unsigned char charValue2 = charPairCounts[i].index - charValue1*26;
+        printf("%d:%c%c: %d\n", charPairCounts[i].index+1, charValue1+'a', charValue2+'a', charPairCounts[i].value);
     }
 
     return 0;
